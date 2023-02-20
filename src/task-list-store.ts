4fa5df3427e45fb089fb729df5748d52a56cbf19
chernenkoik <chernenkoik@tutu.tech>
2023-02-20 17:13:14 +0400

    move task list to task list store

--- src/task-list-store.ts
+++ src/task-list-store.ts
@@ -1,8 +1,31 @@
-import { makeObservable } from 'mobx'
+import { makeAutoObservable } from 'mobx'
 
 class TaskListStore {
+  public tasks = [
+    {
+      title: 'Learn html',
+      id: 0,
+      isDone: false,
+    },
+    {
+      title: 'Learn css',
+      id: 1,
+      isDone: false,
+    },
+    {
+      title: 'Learn js',
+      id: 2,
+      isDone: false,
+    },
+    {
+      title: 'Learn React',
+      id: 3,
+      isDone: false,
+    },
+  ]
+
   constructor() {
-    makeObservable(this)
+    makeAutoObservable(this)
   }
 }
 
--- src/task-list.tsx
+++ src/task-list.tsx
@@ -1,35 +1,13 @@
 import './task-list.css'
 import { Task } from './task'
-
-const taskList = [
-  {
-    title: 'Learn html',
-    id: 0,
-    isDone: false,
-  },
-  {
-    title: 'Learn css',
-    id: 1,
-    isDone: false,
-  },
-  {
-    title: 'Learn js',
-    id: 2,
-    isDone: false,
-  },
-  {
-    title: 'Learn React',
-    id: 3,
-    isDone: false,
-  },
-]
+import { taskListStore } from './task-list-store'
 
 export const TaskList = () => {
   return (
     <div className="taskList">
       <h1>Task list</h1>
       <NewTaskForm />
-      {taskList.map((task) => (
+      {taskListStore.tasks.map((task) => (
         <Task
           key={task.id}
           title={task.title}
@@ -46,26 +24,6 @@ export const NewTaskForm = () => {
     <form
       onSubmit={(event) => {
         event.preventDefault()
-        const form = document.querySelector('form')
-        if (!form) {
-          throw new Error('Form not found!')
-        }
-        const formData = new FormData(form)
-        const title = formData.get('title')
-        const lastId = taskList[taskList.length - 1].id
-        if (typeof title !== 'string') {
-          throw new TypeError('Wrong title format')
-        }
-        taskList.push({
-          title,
-          isDone: false,
-          id: lastId + 1,
-        })
-        const input = document.querySelector('input')
-        if (!input) {
-          throw new Error('Input not found!')
-        }
-        input.value = ''
       }}
     >
       <input placeholder="title" name="title" />
