f76726d1fe7409743f1377ce3c6045a2739d5e30
chernenkoik <chernenkoik@tutu.tech>
2023-02-20 16:43:12 +0400

    create task list const

--- src/task-list.tsx
+++ src/task-list.tsx
@@ -1,6 +1,29 @@
 import './task-list.css'
 import { Task } from './task'
 
+const taskList = [
+  {
+    title: 'Learn html',
+    id: 0,
+    isDone: false,
+  },
+  {
+    title: 'Learn css',
+    id: 1,
+    isDone: false,
+  },
+  {
+    title: 'Learn js',
+    id: 2,
+    isDone: false,
+  },
+  {
+    title: 'Learn React',
+    id: 3,
+    isDone: false,
+  },
+]
+
 export const TaskList = () => {
   return (
     <div className="taskList">
